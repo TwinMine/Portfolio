13201e369010128c46f8ce751c4c0572f09ac362
TwinMine <michaelrolo11@gmail.com>
2026-02-04 14:20:45 +0100

    add new project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,10 @@
-import { NavLink } from "react-router-dom";
 import "./projects.css";
 import acoustiFlow from "/src/assets/walkman.jpg";
 import cashStuffing from "/src/assets/cash-stuffing.png";
 import weatherApp from "/src/assets/weather-app.png";
 import pokePalCodex from "/src/assets/pikachu-1.png";
 import todoWave from "/src/assets/todo-list.png"
+import visualNovel from "/src/assets/destroyed-town.png"
 import { useEffect, useState } from "react";
 
 const Projects = () => {
@@ -107,6 +107,26 @@ const Projects = () => {
           </a>
           
         </div>
+
+<div className={showProjectDiv}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://visual-novel-pi.vercel.app/"
+          >
+            <h3 className="project-topic">Visual Novel</h3>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://visual-novel-pi.vercel.app/"
+          >
+            <img src={visualNovel} alt="" className={showPic} />
+          </a>
+          
+        </div>
+
+
       </div>
     </div>
   );
